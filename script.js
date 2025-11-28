ee33725438018bb261a6a7724273104e0611962f
“vitasiempre” <wavelolmu@gmail.com>
2025-11-28 16:22:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+window.addEventListener('DOMContentLoaded', () => {
+  initCaseListGalleries();
+});
+
+function initCaseListGalleries() {
+  const images = document.querySelectorAll('.case-list-item_gallery_item');
+  images.forEach((img) => {
+    img.classList.add('js-enabled');
+    // your logic...
+  });
+}
