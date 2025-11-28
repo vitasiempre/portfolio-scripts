b1352bb7daff6bf9dca851787031871ebe9ba20c
“vitasiempre” <wavelolmu@gmail.com>
2025-11-28 19:01:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
+alert("RAW JS RUNNING");
 console.log("SCRIPT FILE LOADED");
-
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded fired");
-
-  const images = document.querySelectorAll(".case-list-item_gallery_item");
-  console.log("Found images:", images.length);
-
-  images.forEach((img) => {
-    img.classList.add("js-enabled");
-  });
-
-  console.log("Init finished");
-});
