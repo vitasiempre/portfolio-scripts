b00ef21f92ef8959670a798d93a890061fcda373
“vitasiempre” <wavelolmu@gmail.com>
2025-12-02 12:42:06 +0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -2,5 +2,11 @@ $('.swiper').each(function index() {
     this.classList.add('added');
     let swiper = new Swiper(this, {
         slidesPerView: 'auto',
+        speed: 600,
+        mousewheel: {
+            forceToAxis: true,
+        },
+        keyboard: true,
+        freeMode: true
     }
     )});
