f490e253d00a28e98dd706dbc7c5705378d9539a
“vitasiempre” <wavelolmu@gmail.com>
2025-12-02 14:05:54 +0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -9,8 +9,5 @@ $('.swiper').each(function index() {
         keyboard: true,
         freeMode: true,
         spaceBetween: 15,
-        momentumBounceRatio: 0.75,
-        momentumVelocityRatio: 0.75,
-        momentumRatio: 0.75,
     }
     )});
